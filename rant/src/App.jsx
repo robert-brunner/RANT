08b547a61b9ee7281166c462ac0369a48fb9944e
Robert Brunner <brunner.robert.work@gmail.com>
2024-06-20 23:08:12 -0500

    intitial commit

---
+++ rant/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function About() {
+  return <h1>About Page</h1>;
+}
+
+export default App;
